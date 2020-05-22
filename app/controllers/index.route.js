900c558b0f38bf7a696450146daacd80f3c8f2bc
Tiago Pimenta Da Costa <tiagopimcosta@gmail.com>
2020-05-22 19:04:07 +0100

    BasicAppearance

--- app/controllers/index.route.js
+++ app/controllers/index.route.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const lists = require('../models/lists.model');
+const items = require('../models/items.model');
 
 router.get('/', function(request, response){
 	//console.log(request.user);
 	//console.log(request.isAuthenticated());
-	response.set("Content-Type", "text/html");
-	response.render('index', {})
+	if(request.isAuthenticated()){
+		lists.read(request.user.id_user, function(lists){
+			for(let i = 0; i < lists.length; i++){
+				items.read(lists[i].id_list, function(items){
+					lists[i].items = items;
+
+					if(i == lists.length-1){
+						//console.log(lists);
+						response.set("Content-Type", "text/html");
+						response.render('index', {data: lists})
+					}
+				});
+			};	
+		});
+	}else{
+		response.set("Content-Type", "text/html");
+		response.render('index', {})
+	}
 });
 
 module.exports = router;
